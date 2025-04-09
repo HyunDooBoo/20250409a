927ae9a201548ff6d0b7b39d39b99263e60e810e
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2025-04-09 16:41:32 +0900

    Update CalculatorTest.java

--- src/test/java/edu/fisa/ce/CalculatorTest.java
+++ src/test/java/edu/fisa/ce/CalculatorTest.java
@@ -1,13 +1,5 @@
 package edu.fisa.ce;
-import static org.junit.Assert.assertEquals; // JUnit 4
-// 또는
-import static org.junit.jupiter.api.Assertions.assertEquals; // JUnit 5
 
 public class CalculatorTest {
-	@org.junit.Test // JUnit 4
-	// @org.junit.jupiter.api.Test // JUnit 5
-	public void testAddition() {
-		int result = 2 + 3;
-		assertEquals(5, result); // 성공: 5 == 5
-	}
+
 }
